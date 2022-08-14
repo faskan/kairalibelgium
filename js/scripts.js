e724fa2776005676cd5c84726aba4dfa1d2c81c6
faisalkhan <faisalkhanthayub@gmail.com>
2022-08-14 13:32:24 +0200

    captcha draft

--- js/scripts.js
+++ js/scripts.js
@@ -93,3 +93,20 @@ function onClick(e) {
         });
     });
 }
+
+function onSubmit(token) {
+    var http = new XMLHttpRequest();
+    var url = 'https://hmdowpcunl.execute-api.us-east-1.amazonaws.com/Prod/mail';
+    var params = 'orem=ipsum&name=test&token=' + token;
+    http.open('POST', url, true);
+
+//Send the proper header information along with the request
+    http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+    http.onreadystatechange = function() {//Call a function when the state changes.
+        if(http.readyState == 4 && http.status == 200) {
+            alert(http.responseText);
+        }
+    }
+    http.send(params);
+}
