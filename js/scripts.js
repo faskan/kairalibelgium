28dcfcd9a78e00554317ae9480480a40bb8bddd8
faisalkhan <faisalkhanthayub@gmail.com>
2022-08-18 00:00:53 +0200

    slide carousel for past events

--- js/scripts.js
+++ js/scripts.js
@@ -53,6 +53,64 @@
     navbarCollapse();
     // Collapse the navbar when page is scrolled
     $(window).scroll(navbarCollapse);
+
+    var breakpoint = {
+        // Small screen / phone
+        sm: 576,
+        // Medium screen / tablet
+        md: 768,
+        // Large screen / desktop
+        lg: 992,
+        // Extra large screen / wide desktop
+        xl: 1200
+    };
+
+    $(document).ready(function(){
+        $('#slick').slick({
+            autoplay: true,
+            autoplaySpeed: 2000,
+            draggable: true,
+            infinite: true,
+            dots: false,
+            arrows: false,
+            speed: 1000,
+            mobileFirst: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            responsive: [
+                {
+                    breakpoint: breakpoint.sm,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2
+                    }
+                },
+                {
+                    breakpoint: breakpoint.md,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3
+                    }
+                },
+                {
+                    breakpoint: breakpoint.lg,
+                    settings: {
+                        slidesToShow: 4,
+                        slidesToScroll: 4
+                    }
+                },
+                {
+                    breakpoint: breakpoint.xl,
+                    settings: {
+                        slidesToShow: 5,
+                        slidesToScroll: 5
+                    }
+                }
+            ]
+        });
+    });
+
+
 })(jQuery); // End of use strict
 
 document.addEventListener("DOMContentLoaded", function () {
