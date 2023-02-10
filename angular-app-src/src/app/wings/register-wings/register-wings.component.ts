7b32acc2e3001a0199275c821b44a194d3cff86c
faisalkhan <faisalkhanthayub@gmail.com>
2023-02-10 17:03:35 +0100

    new app

---
+++ angular-app-src/src/app/wings/register-wings/register-wings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-wings',
+  templateUrl: './register-wings.component.html',
+  styleUrls: ['./register-wings.component.css']
+})
+export class RegisterWingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ angular-app-src/src/app/wings/wings.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RegisterWingsComponent } from './register-wings/register-wings.component';
+
+
+
+@NgModule({
+  declarations: [
+    RegisterWingsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class WingsModule { }
