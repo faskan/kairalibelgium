c69a4716648d2cfeb7f70880f73a41d20c11c917
faskan <faisalkhanthayub@gmail.com>
2023-02-10 18:03:39 +0000

    changes from mobile

--- angular-app-src/src/app/app-routing.module.ts
+++ angular-app-src/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterWingsComponent } from './wings/register-wings/register-wings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '**',
+    component: RegisterWingsComponent
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
