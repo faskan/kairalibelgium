5a7824008bffbd35178f4efed6d4dee9137da967
faisalkhan <faisalkhanthayub@gmail.com>
2023-03-24 14:56:13 +0100

    add login component

--- angular-app-src/src/app/wings/login/login.component.ts
+++ angular-app-src/src/app/wings/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { GoogleLoginProvider, SocialAuthService, SocialUser } from '@abacritt/angularx-social-login';
 
 @Component({
   selector: 'app-login',
@@ -6,4 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  user?: SocialUser;
+  loggedIn: boolean = false;
+
+  constructor(@Inject(WINDOW) public window: Window,
+              private authService: SocialAuthService){
+  }
+
+  ngOnInit() {
+    window.googleLoginCallback = (response: any) => {
+      console.log('My new TEst Method');
+      console.log(this.user);
+      this.authService.getAccessToken(GoogleLoginProvider.PROVIDER_ID).then(response => console.log(response));
+    }
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      this.loggedIn = (user != null);
+    });
+  }
 }
