a7301a037532974154f75dc2877c86c87c905f40
faisalkhan <faisalkhanthayub@gmail.com>
2024-09-16 20:38:46 +0200

    program schedule - auto refresh and scroll to top ongoing event

--- angular-app-src/src/app/program-schedule/program-schedule-view-only/program-schedule-view-only.component.ts
+++ angular-app-src/src/app/program-schedule/program-schedule-view-only/program-schedule-view-only.component.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
 import { Schedule } from '../schedule.interface';
-import { HttpClient } from '@angular/common/http';
 import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute } from '@angular/router';
-import { EditScheduleDialogComponent } from '../edit-schedule-dialog/edit-schedule-dialog.component';
 import { ScheduleManagementService } from '../schedule-management.service';
 
 @Component({
@@ -18,6 +16,9 @@ export class ProgramScheduleViewOnlyComponent {
   cancelledSchedules: Schedule[] = [];
   scheduleModel: Schedule = {} as Schedule;
   isLoading = false;
+  autoRefresh = false;
+  refreshInterval = 5;
+  autoRefreshIntervalId: any;
   constructor(private scheduleManagementService: ScheduleManagementService,
               public dialog: MatDialog,
               private route: ActivatedRoute) {
@@ -26,8 +27,23 @@ export class ProgramScheduleViewOnlyComponent {
   ngOnInit(): void {
     this.selectedEventId = this.route.snapshot.paramMap.get('eventId');
     this.loadSchedules();
+    const refreshParam = this.route.snapshot.queryParamMap.get('refreshInterval');
+    this.refreshInterval = refreshParam ? Number(refreshParam) : 5;
+    console.log('Refresh interval:', this.refreshInterval);
+    // Set up auto-refresh
+    this.autoRefreshIntervalId = setInterval(() => {
+      if (this.autoRefresh) {
+        this.loadSchedules();
+      }
+    }, this.refreshInterval * 1000); // Refresh every 5 seconds
   }
 
+  ngOnDestroy(): void {
+    // Clear the interval when the component is destroyed
+    if (this.autoRefreshIntervalId) {
+      clearInterval(this.autoRefreshIntervalId);
+    }
+  }
   loadSchedules() {
     if (this.selectedEventId) {
       this.isLoading = true;
@@ -37,6 +53,14 @@ export class ProgramScheduleViewOnlyComponent {
             this.activeSchedules = data.activeSchedules;
             this.cancelledSchedules = data.cancelledSchedules;
             this.isLoading = false;
+
+            const ongoingScheduleIndex = this.activeSchedules.findIndex(schedule => schedule.status.toLowerCase() === 'ongoing');
+
+            // If an ongoing schedule is found, scroll to it
+            if (ongoingScheduleIndex !== -1) {
+              const element = document.getElementById('schedule-' + ongoingScheduleIndex);
+              if (element) element.scrollIntoView();
+            }
           },
           error: (error) => {
             console.error('Error loading schedules:', error);
